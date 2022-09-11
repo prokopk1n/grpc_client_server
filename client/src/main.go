33b9913f22fce7977e10d005568be086da440641
Sergei Prokopkin <theprokopkin@mail.ru>
2022-09-11 11:48:07 +0300

    Some fixes

--- client/src/main.go
+++ client/src/main.go
@@ -17,18 +17,14 @@ const (
 	postgresConn = "user=postgres password=password dbname=clientdb sslmode=disable"
 )
 
-var db *sql.DB
-var conn *grpc.ClientConn
-
 func main() {
-	var err error
-	conn, err = grpc.Dial("localhost:8081", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.Dial("localhost:8081", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalln("can not connect to localhost")
 	}
 	defer conn.Close()
 
-	db, err = sql.Open("postgres", postgresConn)
+	db, err := sql.Open("postgres", postgresConn)
 	if err != nil {
 		log.Fatalf("Can not connect to : %v", err)
 	}
@@ -36,8 +32,6 @@ func main() {
 
 	handlerManager := handlers.NewHandlerManager(conn, db, time.Second*10, time.Hour)
 	mux := handlerManager.Init()
-
-	fmt.Println("Client: start working...")
 	srv := &http.Server{
 		Addr:    "localhost:8080",
 		Handler: mux,
@@ -46,6 +40,7 @@ func main() {
 			PreferServerCipherSuites: true,
 		},
 	}
+	fmt.Println("Client: start working...")
 	err = srv.ListenAndServeTLS("../key/server.crt", "../key/server.key")
 	if err != nil {
 		log.Fatalln("can not listen port 8080: %v", err)
